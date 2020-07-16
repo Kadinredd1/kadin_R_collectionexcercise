e876461c681bdaccc4a28fa462ad89985e70ec55
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-07-16 01:46:16 -0500

    Add files via upload

---
+++ CollectionExcercise .playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+
+
+
+
+var shoeCollection: [String] = ["Royal Toe Air Jordan 1s" , "Pure Money Jordan 4s", "Raptor Jordan 4s", "Chicago Air Jordan 1s", "Shattered Backboard Jordan 1s", "Bred Jordan 11s", "Dior Chuck Taylors","Adidas x Raf Simons Black Chrome Ozweego", "Nike Air Force 1 '13"]
+
